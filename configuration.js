53ff9294e376a66cb8b1e3fb8c60d097c8136562
ThUnser <thomas.charrayre@gmail.com>
2026-04-14 17:12:22 +0200

    Update configuration.js to add CV link

--- configuration.js
+++ configuration.js
@@ -1,42 +1,12 @@
-const config = {
-  name: "Thomas Charrayre",
-  description: {
-    EN: {
-      short: `Former student of the École Normale Supérieure Ulm, with a master’s degree in political science from Sciences Po 
-      and a master’s degree in political philosophy from the Université Libre de Bruxelles. 
-      Thomas Charrayre is a doctoral candidate in political theory at CEVIPOF – Sciences Po.
-      His research focuses on the intellectual history of conservatism, particularly on the relationship between 
-      conservative thinkers and liberalism, as well as the plurality of their epistemological approaches. 
-      He's affiliated with <ahref="https://www.sciencespo.fr/cevipof/fr.html" target="_blank"> CEVIPOF</a>.
-`,
-      long: "",
-    },
-    FR: {
-      short: `Ancien élève de l’École Normale Supérieure de la rue d’Ulm, 
-      titulaire d’un master de science politique à Sciences Po et d’un master en philosophie politique à l’Université Libre de Bruxelles.
-      Thomas Charrayre est doctorant contractuel en théorie politique au sein du CEVIPOF.
-      Ses travaux portent sur l’histoire intellectuelle du conservatisme, en particulier sur le rapport des penseurs conservateurs au libéralisme 
-      et sur la pluralité de leurs approches épistémologiques.
-      Il est affiliée au <ahref="https://www.sciencespo.fr/cevipof/fr.html" target="_blank"> CEVIPOF</a>.`,
-      long: "",
-    },
-  },
-  links: [
-    {
-      label: "Twitter",
-      value: "@ThomasUnser",
-      url: "https://x.com/ThomasUnser",
-    },
-    {
-      label: "Google Scholar",
-      value: "Thomas Charrayre",
-      url: "https://scholar.google.com/citations?user=usDlHMkAAAAJ&hl=en",
-    },
-    {
-      label: "LinkedIn",
-      value: "Thomas Charrayre",
-      url: "https://www.linkedin.com/in/thomas-charrayre-830716369/",
-    },
-  ],
-  email: { name: "thomas.charrayre", domain: "sciencespo.fr" },
+const configuration = {
+    links: [
+        {
+            label: "CV",
+            value: "Download",
+            url: "./cv.html"
+        },
+        // other existing link objects here...
+    ]
 };
+
+export default configuration;
